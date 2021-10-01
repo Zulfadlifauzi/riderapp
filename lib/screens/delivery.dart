4812670c219bd17fa987b3cb30639a00a3d4c6c9
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-10-01 10:30:09 +0800

    screens

---
+++ lib/screens/delivery.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeliveryScreen extends StatefulWidget {
+  const DeliveryScreen({Key? key}) : super(key: key);
+
+  @override
+  _DeliveryScreenState createState() => _DeliveryScreenState();
+}
+
+class _DeliveryScreenState extends State<DeliveryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
